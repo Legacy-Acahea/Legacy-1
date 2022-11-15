5d3b415eaa6b90b855d133b142f8f416a57dc865
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-15 11:23:26 -0600

    Update Legacy_Version_Info.lua

--- Legacy_Version_Info.lua
+++ Legacy_Version_Info.lua
@@ -1,2 +1,2 @@
-return {{[ [[Version]] ] = [[0.6.8]],[ [[msg]] ] = [[]],},
+return {{[ [[Version]] ] = [[0.6.10]],[ [[msg]] ] = [[Version .6.10 is now live, please click here to download it.]],},
 }
