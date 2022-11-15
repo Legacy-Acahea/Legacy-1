f6e8a23314c5245c643970a64c5957f1bd37a796
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-15 00:11:33 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,4 +1,4 @@
 return {{[ [[Trades]] ] = {2},[ [[Sailing]] ] = {3},},
 {[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Legacy%20Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
-{[ [[link]] ] = [[]],[ [[desc]] ] = [[Legacy Sailing Package, STILL A WORK IN PROGRESS DO NOT DOWNLOAD]],},
+{[ [[link]] ] = [[]],[ [[desc]] ] = [[Legacy Sailing Package, STILL A WORK IN PROGRESS DO NOT USE]],},
 }
