78f54a03d3a5a383bc35bd261251681eb9014e9e
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-17 00:58:14 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,5 +1,5 @@
 return {{[ [[Trades]] ] = {2},[ [[DDB]] ] = {3},[ [[Sailing]] ] = {4},},
-{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Legacy%20Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
+{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
 {[ [[link]] ] = [[]],[ [[desc]] ] = [[Description Database, works with Denizens/Objects/Rooms/Players]],},
 {[ [[link]] ] = [[]],[ [[desc]] ] = [[Legacy Sailing Package]],},
 }
