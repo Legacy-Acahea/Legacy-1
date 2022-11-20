adb5dfd062ee25f0c5c2394130864605e86f6dbc
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-20 14:03:26 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,5 +1,5 @@
 return {{[ [[Trades]] ] = {2},[ [[DDB]] ] = {3},[ [[Sailing]] ] = {4},},
-{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
-{[ [[link]] ] = [[]],[ [[desc]] ] = [[Description Database, works with Denizens/Objects/Rooms/Players]],},
-{[ [[link]] ] = [[]],[ [[desc]] ] = [[Legacy Sailing Package]],},
+{[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Updated as of 11/20/22. Added more gags, fixed the queue breaking on not enough reagents.]],},
+{[ [[link]] ] = [[]],[ [[desc]] ] = [[This is not out yet...soon(tm)]],},
+{[ [[link]] ] = [[]],[ [[desc]] ] = [[This is not out yet...soon(tm)]],},
 }
