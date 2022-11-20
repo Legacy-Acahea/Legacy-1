51d6c981c820f664c6db39ac75e3fc77301cd868
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-20 14:09:30 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,5 +1,5 @@
 return {{[ [[Trades]] ] = {2},[ [[DDB]] ] = {3},[ [[Sailing]] ] = {4},},
-{[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Updated as of 11/20/22. Added more gags, fixed the queue breaking on not enough reagents.]],},
+{[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Updated as of 11/20/22. Added more gags, fixed the queue breaking on not enough reagents. Augmentation is still getting fixed up.]],},
 {[ [[link]] ] = [[]],[ [[desc]] ] = [[This is not out yet...soon(tm)]],},
 {[ [[link]] ] = [[]],[ [[desc]] ] = [[This is not out yet...soon(tm)]],},
 }
