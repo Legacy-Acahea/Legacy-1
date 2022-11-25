ba02216ab6858907aa56b0bec310d1a9162f2cda
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-24 23:49:12 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,5 +1,5 @@
 return {{[ [[Trades]] ] = {2},[ [[Limb Tracker]] ] = {3},[ [[Shrines]] ] = {4},},
-{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Legacy%20Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
+{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
 {[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker]],},
 {[ [[link]] ] = [[]],[ [[desc]] ] = [[Legacy Shrine Tracker]],},
 }
