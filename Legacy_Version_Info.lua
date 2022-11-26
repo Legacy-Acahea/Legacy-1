d39cf2d2f46053f28ec98351d262aa162f69db51
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-11-25 20:06:30 -0600

    Update Legacy_Version_Info.lua

--- Legacy_Version_Info.lua
+++ Legacy_Version_Info.lua
@@ -1,2 +1,2 @@
-return {{[ [[msg]] ] = [[Click here to update to<gold> Legacy V]],[ [[Version]] ] = [[0.5]],[ [[link]] ] = [[]],},
+return {{[ [[msg]] ] = [[Click here to update to<gold> Legacy V]],[ [[Version]] ] = [[0.8]],[ [[link]] ] = [[]],},
 }
