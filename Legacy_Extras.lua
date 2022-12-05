6ac8b434a8ec9149738e6c4e506938f7910cc058
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-12-04 21:58:41 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,5 +1,5 @@
 return {{[ [[Trades]] ] = {2},[ [[Limb Tracker]] ] = {3},[ [[Basher]] ] = {4},},
 {[ [[link]] ] = [[https://github.com/Legacy-Achaea/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
 {[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker, please support our british pal]],},
-{[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Early phase Bashing package]],},
+{[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Newest Version as of 12.4.22, Now includes rage information for every class. This will need more information for Elemental Lords. If you installed a previous version you will want to do: lua Legacy.Settings.Basher.Classes = {}. You will have to reset your class attack, prealias and now non-rage shield attack. Sorry for the trouble.]],},
 }
