c274043aacec26447cdbb176129008400368243d
AranosBanazir <115073504+AranosBanazir@users.noreply.github.com>
2022-12-04 18:33:38 -0600

    Update Legacy_Extras.lua

--- Legacy_Extras.lua
+++ Legacy_Extras.lua
@@ -1,5 +1,5 @@
 return {{[ [[Trades]] ] = {2},[ [[Limb Tracker]] ] = {3},[ [[Basher]] ] = {4},},
-{[ [[link]] ] = [[https://github.com/AranosBanazir/Legacy/raw/main/Legacy%20Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
+{[ [[link]] ] = [[https://github.com/Legacy-Achaea/Legacy/raw/main/Trades.mpackage]],[ [[desc]] ] = [[Legacy Trades Package for: Conjuration, Inkmilling, Augmentation]],},
 {[ [[link]] ] = [[https://github.com/27theo/limb/releases/download/v1.2/limb.1.2.mpackage]],[ [[desc]] ] = [[Romaen's Limb tracker, please support our british pal]],},
 {[ [[link]] ] = [[https://github.com/Legacy-Acahea/Legacy/raw/main/Basher.mpackage]],[ [[desc]] ] = [[Early phase Bashing package]],},
 }
